724f88cffe9dc4cbd12a2668c499fbfadc9356fc
srividhya2610 <srividhya.ravindran@testpro.io>
2023-05-04 22:04:25 -0700

    Homework22

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import java.time.Duration;
@@ -12,9 +13,9 @@ import java.util.UUID;
 public class BaseTest {
 
     public static WebDriver driver = null;
-
+    public static WebDriverWait wait;
     public static String url = "";
-
+    public static WebDriver actions;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -39,6 +40,7 @@ public class BaseTest {
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver,Duration.ofSeconds(4));
         url = BaseURL;
         navigateToPage();
     }
